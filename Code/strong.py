13c805e19ea8302a3371ad8d1d4f2049b6b6a5d7
Alexandros Kyriakakis <alexanders_mac@MacBook-Pro-Alexandros.local>
2021-09-15 22:46:31 +0300

    Made chart total_volume_per_exercise

--- Code/strong.py
+++ Code/strong.py
@@ -41,79 +41,124 @@ def OneRepMax(weight,reps):
    return int(weight/percentage)
 
 # Total Volume
-def total_volume():
+def total_volume(with_plot = True):
    volume_of_every_workout = []
    current_date = all_workouts[0]['Date']
    total_volume_of_current_workout = 0
    for workout in all_workouts:
-      total_volume_of_current_workout += workout["Weight"] * workout["Reps"] 
       if workout['Date'] != current_date:
          volume_of_every_workout.append({'Date':current_date,'Total Volume':total_volume_of_current_workout})
          total_volume_of_current_workout = 0
          current_date = workout['Date']
+      total_volume_of_current_workout += workout["Weight"] * workout["Reps"] 
    if total_volume_of_current_workout != 0:
       volume_of_every_workout.append({'Date':current_date,'Total Volume':total_volume_of_current_workout})
-   x_axis = []
-   y_axis = []
-   for i in volume_of_every_workout:
-      x_axis.append( dt.datetime.fromisoformat( i["Date"] ))
-      y_axis.append( i["Total Volume"] )
+   if with_plot:
+      x_axis = []
+      y_axis = []
+      for i in volume_of_every_workout:
+         x_axis.append( dt.datetime.fromisoformat( i["Date"] ))
+         y_axis.append( i["Total Volume"] )
 
-   clist = [(0, "red"), (0.125, "red"), (0.25, "orange"), (0.5, "green"), 
-            (0.7, "green"), (0.75, "blue"), (1, "blue")]
-   rvb = mcolors.LinearSegmentedColormap.from_list("", clist)
-   color=rvb(np.array(y_axis)/max(y_axis))
-
-   plt.bar(x_axis,y_axis,width = 0.9, color=color)
-   plt.scatter(x_axis,y_axis, color=color)
-   plt.title("Total volume per Workout")
-   plt.xticks(rotation=45)
-   plt.grid(axis='y', which='both')
-   plt.ylabel("Total Volume")
-   plt.xlabel("Date")
-   plt.show()   
-   
-   plt.cla()
-   plt.clf()
+      clist = [(0, "red"), (0.125, "red"), (0.25, "orange"), (0.5, "green"), 
+               (0.7, "green"), (0.75, "blue"), (1, "blue")]
+      rvb = mcolors.LinearSegmentedColormap.from_list("", clist)
+      color=rvb(np.array(y_axis)/max(y_axis))
+      plt.style.use('dark_background')
+      plt.bar(x_axis,y_axis,width = 0.9, color=color)
+      plt.scatter(x_axis,y_axis, color=color)
+      plt.title("Total volume per Workout")
+      plt.xticks(rotation=45)
+      plt.grid(axis='y', which='both')
+      plt.ylabel("Total Volume")
+      plt.xlabel("Date")
+      plt.show()   
+      
+      plt.cla()
+      plt.clf()
+   return volume_of_every_workout
 
 #total_volume()
+
+
 # Best set over time per given exercise
-def best_set(exercise_name = "Snatch (Barbell)"):
+def best_set(exercise_name = "Snatch (Barbell)", with_plot = True):
    best_sets = []
    current_date = all_workouts[0]['Date']
    current_best_set = 0
    for workout in all_workouts:
       if workout["Exercise Name"] != exercise_name:
          continue
-      if current_best_set <= OneRepMax(workout['Weight'],workout['Reps']):
-         current_best_set = OneRepMax(workout['Weight'],workout['Reps'])
       if workout['Date'] != current_date:
          best_sets.append({"Date":current_date, "1RM":current_best_set})
          current_best_set = 0
          current_date = workout["Date"]
+      if current_best_set <= OneRepMax(workout['Weight'],workout['Reps']):
+         current_best_set = OneRepMax(workout['Weight'],workout['Reps'])
    if current_best_set != 0:
       best_sets.append({"Date":current_date, "1RM":current_best_set})
+   if with_plot:
+      x_axis = []
+      y_axis = []
+      for i in best_sets:
+         x_axis.append( dt.datetime.fromisoformat( i["Date"] ))
+         y_axis.append( i["1RM"] )
+      plt.style.use('dark_background')
+      plt.plot(x_axis,y_axis, color="red", linewidth = 2,
+            marker='.', markerfacecolor='blue', markersize=10)
+      plt.title("Best set of {} per Workout".format(exercise_name))
+      plt.xticks(rotation=45)
+      plt.grid()
+      plt.ylabel("Best Set")
+      plt.xlabel("Date")
+      plt.show()   
+      
+      plt.cla()
+      plt.clf()
+   return best_sets
+#best_set()
 
-   x_axis = []
-   y_axis = []
-   for i in best_sets:
-      x_axis.append( dt.datetime.fromisoformat( i["Date"] ))
-      y_axis.append( i["1RM"] )
 
-   plt.plot(x_axis,y_axis, color="red", linewidth = 2,
-         marker='.', markerfacecolor='blue', markersize=10)
-   plt.title("Best set of {} per Workout".format(exercise_name))
-   plt.xticks(rotation=45)
-   plt.grid()
-   plt.ylabel("Best Set")
-   plt.xlabel("Date")
-   plt.show()   
-   
-   plt.cla()
-   plt.clf()
-best_set()
 # Total volume over time per given exercise
+def total_volume_per_exercise(exercise_name = "Snatch (Barbell)", with_plot = True):
+   volume_of_every_set = []
+   current_date = all_workouts[0]['Date']
+   current_sets_volume = 0
+   for workout in all_workouts:
+      if workout["Exercise Name"] != exercise_name:
+         continue
+      if workout['Date'] != current_date:
+         volume_of_every_set.append({"Date":current_date, "Volume":current_sets_volume})
+         current_sets_volume = 0
+         current_date = workout["Date"]
+      current_sets_volume += workout["Weight"] * workout["Reps"] 
+   if current_sets_volume != 0:
+      volume_of_every_set.append({"Date":current_date, "Volume":current_sets_volume})
+   if with_plot:
+      x_axis = []
+      y_axis = []
+      for i in volume_of_every_set:
+         x_axis.append( dt.datetime.fromisoformat( i["Date"] ))
+         y_axis.append( i["Volume"] )
+      plt.style.use('dark_background')
+      plt.plot(x_axis,y_axis, color="red", linewidth = 2,
+            marker='.', markerfacecolor='blue', markersize=10)
+      plt.title("Volume of {} per Workout".format(exercise_name))
+      plt.xticks(rotation=45)
+      plt.grid()
+      plt.ylabel("Volume")
+      plt.xlabel("Date")
+      plt.show()   
+      
+      plt.cla()
+      plt.clf()
+   return volume_of_every_set
+#total_volume_per_exercise()
 
-# PR progresion over time per given exercise
+
+
+# PR progression over time per given exercise
+def PR_progression(exercise_name = "Snatch (Barbell)", with_plot = True):
+   
 
 # Max consecutive reps 
